a8d22ad1195aa2a21230d126cdcf9f10fc8580d4
TlokotseSM <tlokotsemogudi@gmail.com>
2025-10-16 22:37:08 +0200

    introduced RolesGuard to control access based on user roles

---
+++ src/modules/auth/guards/roles.guard.ts
@@ -0,0 +1,34 @@
+import { Injectable, CanActivate, ExecutionContext, ForbiddenException } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { ROLES_KEY } from '../decorators/roles.decorator';
+import { UserRole } from 'src/shared/database/entities/user.entity';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const requiredRoles = this.reflector.getAllAndOverride<UserRole[]>(ROLES_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (!requiredRoles) {
+      return true;
+    }
+
+    const { user } = context.switchToHttp().getRequest();
+
+    if (!user) {
+      throw new ForbiddenException('User not found');
+    }
+
+    if (!requiredRoles.includes(user.role)) {
+      throw new ForbiddenException(
+        `User role "${user.role}" does not have access to this resource. Required: ${requiredRoles.join(', ')}`,
+      );
+    }
+
+    return true;
+  }
+}
